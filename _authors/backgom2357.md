68425d36da218f1ead33fb986d05145cc261c15f
backgom2357 <backgom2357@gmail.com>
2020-04-10 22:37:22 +0900

    ver 2.0

--- _authors/backgom2357.md
+++ _authors/backgom2357.md
@@ -6,16 +6,13 @@ email: backgom2357@gmail.com
 github_url: "https://github.com/backgom2357"
 blog_url:
 ---
-### Polar B
-![avatar2](https://avatars2.githubusercontent.com/u/30210944?s=400&v=4){: width="360" height="auto"}
+
+<b>Links</b>
+<br>
+email : backgom2357@gmail.com
+<br>
+github : <https://github.com/backgom2357>
+<br>
+facebook : <https://www.facebook.com/backgom2357>
 <br><br>
-sy0277@naver.com <br>
-backgom2357@gmail.com
-<br><br>
-Team: Unsolved Problem  
-<br><br>
-수학, 데이터 사이언스, 프로그래밍, 파이썬, 머신러닝, 딥러닝, 유니티(C#), C++, 자바, 게임 개발자
-<br><br>
-Mathematics, Data Science, Programing, Python, Machine Learning, Deep Learning, Unity(C#), C++, Java, Game Developer
-<br><br>
-Hands-on Machine Learning with Scikit-Learn and TensorFlow, First Contact with Tensorflow, Project
+Mathematics, Python, Deep Learning, Unity(C#), C++, Reinforcement Learning
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,191 +1,111 @@
-"use strict";
+/*  ---------------------------------------------------
+  Template Name: Hazze
+  Description: Manup Fashion HTML Template
+  Author: Colorlib
+  Author URI: http://colorlib.com
+  Version: 1.0
+  Created: Colorlib
+---------------------------------------------------------  */
 
-//Rage Template
-//Designerd by: http://bootstrapthemes.co
+'use strict';
 
-jQuery(document).ready(function ($) {
+(function ($) {
 
-//for Preloader
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
 
-    $(window).load(function () {
-        $("#loading").fadeOut(500);
-    });
-
-
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: (target.offset().top - 80)
-                }, 1000);
-                if ($('.navbar-toggle').css('display') != 'none') {
-                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
-                }
-                return false;
-            }
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.gallery-controls li').on('click', function () {
+            $('.gallery-controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.gallery-filter').length > 0) {
+            var containerEl = document.querySelector('.gallery-filter');
+            var mixer = mixitup(containerEl);
         }
-    });
-
-
-
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
-
-    var wow = new WOW({
-        mobile: false // trigger animations on mobile devices (default is true)
-    });
-    wow.init();
-
-// magnificPopup
-
-    $('.popup-img').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-
-    $('.video-link').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        iframe: {
-  markup: '<div class="mfp-iframe-scaler">'+
-            '<div class="mfp-close"></div>'+
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-  prerns: {
-    youtube: {
-      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-      id: 'v=', // String that splits URL in a two parts, second part should be %id%
-      // Or null - full URL will be returned
-      // Or a function that should return %id%, for example:
-      // id: function(url) { return 'parsed id'; }
-
-      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
-    }
-    // you may add here more sources
-
-  },
-
-  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-}
 
     });
 
-
-
-// slick slider active Home Page Tow
-    $(".testimonial_slid").slick({
-        dots: false,
-        infinite: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: "<i class='fa fa-angle-left nextprevleft'></i>",
-        nextArrow: "<i class='fa fa-angle-right nextprevright'></i>",
-        autoplay: true,
-        autoplaySpeed: 2000
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
     });
 
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
 
-
-//    featured slider
-    $('.featured_slider').slick({
-        centerMode: true,
-        dote: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        speed: 1500,
-        index: 2,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
+    /*------------------------
+		Testimonial Slider
+    ----------------------- */
+    $(".testimonial-slider").owlCarousel({
+        items: 2,
+        dots: true,
+        // autoplay: true,
+        loop: true,
+        smartSpeed: 1200,
+        margin: 0,
+        responsive: {
+            320: {
+                items: 1,
             },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
+            480: {
+                items: 1,
+            },
+            992: {
+                items: 2,
             }
-        ]
-    });
-
-
-
-//---------------------------------------------
-// Counter
-//---------------------------------------------
-
-    $('.statistic-counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-//---------------------------------------------
-// Scroll Up
-//---------------------------------------------
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 600) {
-            $('.scrollup').fadeIn('slow');
-        } else {
-            $('.scrollup').fadeOut('slow');
-        }
-    });
-    $('.scrollup').click(function () {
-        $("html, body").animate({scrollTop: 0}, 1000);
-        return false;
-    });
-
-
-
-
-
-//About us accordion
-
-    $("#faq_main_content").collapse({
-        accordion: true,
-        open: function () {
-            this.addClass("open");
-            this.css({height: this.children().outerHeight()});
-        },
-        close: function () {
-            this.css({height: "0px"});
-            this.removeClass("open");
         }
     });
 
-
-
-
-
-//Team Skillbar active js
-
-    jQuery('.teamskillbar').each(function () {
-        jQuery(this).find('.teamskillbar-bar').animate({
-            width: jQuery(this).attr('data-percent')
-        }, 6000);
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.image-popup').magnificPopup({
+        type: 'image'
     });
 
+    /*------------------
+        Counter Up
+    --------------------*/
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 
+    /*------------------
+        About Counter Up
+    --------------------*/
+    $('.ab-count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
 
-    //End
-
-});
+})(jQuery);
